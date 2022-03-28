26255bb1f362bb34c7673953b50a63877ea3b43b
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-03-28 13:56:28 -0300

    Feature: method Post api complete

--- src/pages/api/user.jsx
+++ src/pages/api/user.jsx
@@ -4,15 +4,22 @@ export default async (req,res) => {
     const { db } = await connect();
 
     if(req.method === "POST"){
-        const response = await db.collection("clientes").insertOne({
-            name: "Rogério",
-            age: 17,
-        });
-    
-        res.status(200).json(response);    
+        const  {name, cellphone, email, password} = req.body;
+
+        if(!name || !cellphone || !email || !password){
+            res.status(400).json({message: "Error 400, Missing body parameter"});
+        }
+        else{
+            const response = await db.collection("clientes").insertOne({
+                name: name,
+                cellphone: cellphone,
+                email: email,
+                password: password
+            });
+            res.status(200).json(response);
+        } 
     }
     else{
-        const { name } = req.body;
-        res.status(400).json({mensage: name})
+        res.status(400).json({message: "Error 400, Use Post method"})
     }
 }
