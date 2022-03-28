ee8006f98aea8cb7b8d76b3dd4bfd868388dcfad
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-03-28 15:18:51 -0300

    Starting new feature with Auth0

---
+++ src/pages/_app.jsx
@@ -0,0 +1,13 @@
+// pages/_app.js
+import { SessionProvider } from "next-auth/react"
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,19 @@
-import connect from "../../utils/database";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home() {
-
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>
-
-      <h1>Ola Mundo</h1>
-
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
