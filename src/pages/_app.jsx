ee85f1f7a73cfb3357c62f3540198cf98b81b233
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-03-29 14:55:27 -0300

    Fix Auth0 Error

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,10 @@
-// pages/_app.js
-import { SessionProvider } from "next-auth/react"
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <UserProvider>
       <Component {...pageProps} />
-    </SessionProvider>
-  )
+    </UserProvider>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,8 @@
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+export default function Home() {
+  
+  return(
+    <div>
+      <a href="/api/auth/login">Login</a>
+    </div>
   )
 }
