a09c31d5ee3dd0b5cb868ccc0ef8d57f34975159
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-03-30 15:47:07 -0300

    Fix Error: error syntax too

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
 import { useUser } from '@auth0/nextjs-auth0';
+import Link from "next/link";
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading... </div>;
   if (error) return <div>{error.message}</div>;
-  if (!user) return <div><a href="/api/auth/login">Login</a></div>
+  if (!user) return <div><Link href="/api/auth/login">Login</Link></div>
 
   return (
     user && (
@@ -13,7 +14,7 @@ export default function Profile() {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <a href="/api/auth/logout">Logout</a>
+        <Link href="/api/auth/logout">Logout</Link>
       </div>
     )
   );
