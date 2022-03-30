1a06780b59f47f2a5f7df4bda70b34c3754db89a
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-03-30 15:38:34 -0300

    New Feture: Make a login session

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,20 @@
-export default function Home() {
-  
-  return(
-    <div>
-      <a href="/api/auth/login">Login</a>
-    </div>
-  )
+import { useUser } from '@auth0/nextjs-auth0';
+
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading... </div>;
+  if (error) return <div>{error.message}</div>;
+  if (!user) return <a href="/api/auth/login">Login</a>
+
+  return (
+    user && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  );
 }
