242f699cfbfa3be995b3f8489831e3d12595b5df
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-17 15:22:20 -0300

    Add Global

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
-
-export default function App({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <UserProvider>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </UserProvider>
-  );
+    </SessionProvider>
+  )
 }
