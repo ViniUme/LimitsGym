b05e706f234ef3f79a32e685e07a5d2d28095c88
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-20 16:32:21 -0300

    Add functions in Index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,38 +1,18 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useState } from 'react';
+import { VerifyUser } from '../../lib/functions';
 
 export default function Index() {
 
   const { data: session } = useSession()
-  const [wish, setWish] = useState(0)
 
   if (session) {
-
-    async function VerifyUser(){
-  
-      const email = {
-        email: session.user.email
-      }
-
-      const init = {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(email)
-      }
-
-      await fetch('/api/verify-user', init)
-        .then(response => response.json())
-        .then(json => console.log(json))
-    }
 
     return (
       <>
         Signed in as {session.user.email} <br />
         <img src={session.user.image} alt="" />
         <button onClick={() => signOut()}>Sign out</button>
-        <button onClick={() => VerifyUser()}>add</button>
+        <button onClick={() => VerifyUser({email: session.user.email})}>add</button>
       </>
     )
   }
