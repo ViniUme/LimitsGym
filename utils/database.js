9991c3add5b804f26953bb1ad5a8792306a403d0
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-20 16:34:30 -0300

    Deleting empty line

--- utils/database.js
+++ utils/database.js
@@ -7,7 +7,7 @@ const client = new MongoClient(process.env.DATABASE_URL, {
 
 export default async function Connect(){
     await client.connect();
-    const db = client.db(process.env.DATABASE_NAME);
 
+    const db = client.db(process.env.DATABASE_NAME);
     return {db, client}
 }
