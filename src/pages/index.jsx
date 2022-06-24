3643c67d643ace1bc541e2ededfbd4387341840d
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-24 15:27:38 -0300

    Create index frontend

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,46 @@
-import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import style from '../styles/index.module.scss'
+import Link from 'next/Link';
+import { useState } from 'react';
 
 export default function Index() {
-  return(
-    <div>
 
-      {/* Navbar Start */ }
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* Navbar End */}
+  const [activate, setActivate] = useState(style.navbar);
+
+  const SwitchNavbar = () => {
+    if(activate == style.navbar){
+      setActivate(style.navbar_activate);
+    }
+    else{
+      setActivate(style.navbar);
+    }
+  }
+
+  return(
+    <div className={style.container}>
+
+      <nav className={activate}>
+
+        <div className={style.hamburguer_menu} onClick={() => SwitchNavbar()}>
+          <div className={style.menu_line_1}></div>
+          <div className={style.menu_line_2}></div>
+          <div className={style.menu_line_3}></div>
+        </div>
+
+        <Link href="#"><a className={style.nav_link_main}>início</a></Link>
+        <Link href="#"><a className={style.nav_link}>sobre nós</a></Link>
+        <Link href="#"><a className={style.nav_link}>aula</a></Link>
+        <Link href="#"><a className={style.nav_link}>contatos</a></Link>
+
+      </nav>
+
+      <section className={style.section_main}>
+        <div className={style.wallpaper}></div>
+        <h1 className={style.titles}>
+          <span className={style.title_sub}>exceda seus limites</span>
+          <span className={style.title_header}>academia limits</span>
+        </h1>
+      </section>
+
     </div>
   )
 }
