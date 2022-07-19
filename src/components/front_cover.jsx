48459fd06e49600d235d1be81a83c1d3aac2cc24
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-19 14:05:20 -0300

    Organized front_cover

--- src/components/front_cover.jsx
+++ src/components/front_cover.jsx
@@ -4,14 +4,16 @@ import style from '../styles/front_cover.module.scss';
 export default function FrontCover(){
     return(
         <section className={style.section_main}>
-            <div className={style.wallpaper}></div>
-            <h1 className={style.titles}>
-            <span className={style.title_sub}>exceda seus limites</span>
-            <span className={style.title_header}>academia limits</span>
-            
-            <Link href="#"><a className={style.link_button}><button className={style.title_shadow}><span className={style.title_button}>junte-se a nós</span></button></a></Link>
-
-            </h1>
+            <div className={style.wallpaper}>
+                <span className={style.title_sub}>exceda seus limites</span>
+                <span className={style.title_header}>academia limits</span>
+                
+                <Link href="#">
+                    <a className={style.link_button}>
+                        <button className={style.title_shadow}><span className={style.title_button}>junte-se a nós</span></button>
+                    </a>
+                </Link>
+            </div>
         </section>
     )
 }
