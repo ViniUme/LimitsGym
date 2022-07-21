c6345bb781ac0c2b8f3f599dfc7946aecbb5aea1
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-21 14:32:43 -0300

    Gym_class text

--- src/components/gym_class.jsx
+++ src/components/gym_class.jsx
@@ -8,14 +8,14 @@ export default function GymClass() {
             <div className={style.body}>
                 <h1 className={style.title}>Musculação</h1>
                 <h2 className={style.description}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nobis possimus delectus illum ut numquam pariatur nihil vero, consectetur blanditiis officia similique voluptas, totam culpa perferendis doloribus reiciendis nisi dolorem.
+                    Venha treinar para se manter saldável fisicamente e psicologicamente. A musculação traz vários benefícios como maior qualidade do sono, melhora do humor, prevenção do diabetes, ajuda a emagrecer, entre muitos outros.
                 </h2>
                 <Link href="#"><a className={style.link_join}><button className={style.button_join}>Junte-se agora</button></a></Link>
             </div>
             <div className={style.muscle}>
                 <h1 className={style.title}>Construção de Músculos</h1>
                 <h2 className={style.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia iusto nihil ad? Ad ipsum quasi omnis amet veniam pariatur ea, eveniet, sed magnam id similique sunt deleniti sequi eius? Nobis.
+                    Treine conosco para contruir musculos e conquistar a definição que tanto deseja. Aqui você encontra os melhores aparelhos para seu treino independentemente se você esta começando ou se já é avançado.
                 </h2>
                 <Link href="#"><a className={style.link_join}><button className={style.button_join}>Junte-se agora</button></a></Link>
             </div>
