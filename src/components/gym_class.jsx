ba3493c5fcf641b4a471b08a11762f62473aedbf
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-21 15:04:24 -0300

    Edited text to most performance

--- src/components/gym_class.jsx
+++ src/components/gym_class.jsx
@@ -8,18 +8,17 @@ export default function GymClass() {
             <div className={style.body}>
                 <h1 className={style.title}>Musculação</h1>
                 <h2 className={style.description}>
-                    Venha treinar para se manter saldável fisicamente e psicologicamente. A musculação traz vários benefícios como maior qualidade do sono, melhora do humor, prevenção do diabetes, ajuda a emagrecer, entre muitos outros.
+                    Venha treinar para manter saldável tanto seu físico quanto seu psicológico. A musculação nos traz vários vantagens como uma maior qualidade do sono, melhora do humor, prevenção do diabetes, ajuda a emagrecer, entre muitos outros.
                 </h2>
                 <Link href="#"><a className={style.link_join}><button className={style.button_join}>Junte-se agora</button></a></Link>
             </div>
             <div className={style.muscle}>
                 <h1 className={style.title}>Construção de Músculos</h1>
                 <h2 className={style.description}>
-                    Treine conosco para contruir musculos e conquistar a definição que tanto deseja. Aqui você encontra os melhores aparelhos para seu treino independentemente se você esta começando ou se já é avançado.
+                    Treine conosco para ganhar mais massa muscular para ter a definição que tanto quer. Aqui você encontra os melhores aparelhos para seu treino mesmo se você esta começando ou se já é de nível avançado.
                 </h2>
                 <Link href="#"><a className={style.link_join}><button className={style.button_join}>Junte-se agora</button></a></Link>
             </div>
-
         </div>
     )
 }
