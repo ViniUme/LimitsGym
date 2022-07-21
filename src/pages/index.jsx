85e3d0b2b940404071ada4d6db1355eb08f229cb
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-21 15:15:42 -0300

    Putting front_cover in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,32 @@
 import Navbar from '../components/navbar';
-import FrontCover from '../components/front_cover';
 import GymClass from '../components/gym_class';
 import Page from '../components/page';
 import style from '../styles/index.module.scss';
+import Link from 'next/link';
 
 export default function Index() {
 
   return(
     <Page title="Academia Limits - Exceda seus limites" description="Compra de produtos">
       <div className={style.container}>
+
         <Navbar/>
-        <FrontCover/>
+
+        <section className={style.front_cover}>
+            <div className={style.wallpaper}>
+                <span className={style.title_sub}>exceda seus limites</span>
+                <span className={style.title_header}>academia limits</span>
+                
+                <Link href="#">
+                    <a className={style.link_button}>
+                        <button className={style.title_shadow}><span className={style.title_button}>junte-se a nós</span></button>
+                    </a>
+                </Link>
+            </div>
+        </section>
+
         <GymClass/>
+        
       </div>
     </Page>
   )
