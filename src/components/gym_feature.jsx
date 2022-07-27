76e0d58c7b390271a2f8883a50dfb8e57c3c1e62
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-07-27 15:42:32 -0300

    Fix order and svgs Change order of svgs

--- src/components/gym_feature.jsx
+++ src/components/gym_feature.jsx
@@ -9,18 +9,22 @@ export default function GymFeature(){
             <div className={style.div_features}>
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img1}`} />
+                    <img src='svg/feature-svg1.svg' alt='Saúde' />
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img2}`} />
+                    <img src='svg/feature-svg2.svg' alt='' />
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img3}`} />
+                    <img src='svg/feature-svg3.svg' alt='' />
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img4}`} />
+                    <img src='svg/feature-svg4.svg' alt='' />
                 </div>
             </div>
         </div>
