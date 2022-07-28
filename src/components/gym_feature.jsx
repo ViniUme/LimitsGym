009808d76f4f18054eeb7be1c30146d86edc9bb7
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-07-28 15:23:27 -0300

    New stage Finalized styles and start responsiviness

--- src/components/gym_feature.jsx
+++ src/components/gym_feature.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import style from '../styles/gym_feature.module.scss';
 
 export default function GymFeature(){
@@ -9,22 +10,38 @@ export default function GymFeature(){
             <div className={style.div_features}>
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img1}`} />
-                    <img src='svg/feature-svg1.svg' alt='Saúde' />
+                    <img className={style.svg_feature} src='svg/feature-svg1.svg' alt='Saúde' />
+                    <div className={style.div_text}>
+                        <h4 className={style.title_feature}>Benefício à Saúde</h4>
+                        <h5 className={style.descrition_feature}>Pode prevenir doenças, desde as cardiovasculares, diabetes, hipertensão, osteoporose, entre outros.</h5>
+                    </div>
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img2}`} />
-                    <img src='svg/feature-svg2.svg' alt='' />
+                    <img className={style.svg_feature} src='svg/feature-svg2.svg' alt='' />
+                    <div className={style.div_text}>
+                        <h4 className={style.title_feature}>Benefício à Autoestima</h4>
+                        <h5 className={style.descrition_feature}>Quem começa a praticar atividade física ganha também controle emocional, aumento da autoestima, diminuição de ansiedade e melhora no humor.</h5>
+                    </div>
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img3}`} />
-                    <img src='svg/feature-svg3.svg' alt='' />
+                    <img className={style.svg_feature} src='svg/feature-svg3.svg' alt='' />
+                    <div className={style.div_text}>
+                        <h4 className={style.title_feature}>Maior Condicionamento</h4>
+                        <h5 className={style.descrition_feature}>Quanto mais intenso for a musculação, maior é o trabalho do coração, garantindo maior condicionamento cardíaco e respiratório.</h5>
+                    </div>
                 </div>
 
                 <div className={style.view_feature}>
                     <div className={`${style.img_feature} ${style.img4}`} />
-                    <img src='svg/feature-svg4.svg' alt='' />
+                    <img className={style.svg_feature} src='svg/feature-svg4.svg' alt='' />
+                    <div className={style.div_text}>
+                        <h4 className={style.title_feature}>Trata problemas Emocionais</h4>
+                        <h5 className={style.descrition_feature}>Pro promover a liberação da endorfina, a musculação pode ser uma ótima alternativa para aliviar o estressse, diminuir os sintomas de ansiedade e até mesmo combater a depressão.</h5>
+                    </div>
                 </div>
             </div>
         </div>
