eef35150e35935f47148688ea21985af31ba7155
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-01 16:25:09 -0300

    Created calendar file Created calendar jsx file

---
+++ src/components/calendar.jsx
@@ -0,0 +1,48 @@
+import style from '../styles/calendar.module.scss';
+
+export default function Calendar(){
+    return(
+        <div className={style.calendar}>
+            <div className={style.hourly}>Horas</div>
+            <div className={style.monday}></div>
+            <div className={style.tuesday}></div>
+            <div className={style.wednesday}></div>
+            <div className={style.thursday}></div>
+            <div className={style.friday}></div>
+            <div className={style.saturday}></div>
+            <div className={style.sunday}></div>
+            <div className={style.hour_1}></div>
+            <div className={style.hour_2}></div>
+            <div className={style.hour_3}></div>
+            <div className={style.hour_4}></div>
+            <div className={style.name_1}></div>
+            <div className={style.name_2}></div>
+            <div className={style.name_3}></div>
+            <div className={style.name_4}></div>
+            <div className={style.name_5}></div>
+            <div className={style.name_6}></div>
+            <div className={style.name_7}></div>
+            <div className={style.name_8}></div>
+            <div className={style.name_9}></div>
+            <div className={style.name_10}></div>
+            <div className={style.name_11}></div>
+            <div className={style.name_12}></div>
+            <div className={style.name_13}></div>
+            <div className={style.name_14}></div>
+            <div className={style.name_15}></div>
+            <div className={style.name_16}></div>
+            <div className={style.name_17}></div>
+            <div className={style.name_18}></div>
+            <div className={style.name_19}></div>
+            <div className={style.name_20}></div>
+            <div className={style.name_21}></div>
+            <div className={style.name_22}></div>
+            <div className={style.name_23}></div>
+            <div className={style.name_24}></div>
+            <div className={style.name_25}></div>
+            <div className={style.name_26}></div>
+            <div className={style.name_27}></div>
+            <div className={style.name_28}></div>
+        </div>
+    );
+}
