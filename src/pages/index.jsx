2c3b4ae2d161bae58c9963e3d61dc3a16002f21a
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-17 13:07:25 -0300

    Test login

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,11 @@ import Calendar from '../components/calendar';
 import Page from '../components/page';
 import style from '../styles/index.module.scss';
 
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Index() {
+
+  const { data: session } = useSession();
 
   return(
     <Page title="Academia Limits - Exceda seus limites" description="Compra de produtos">
@@ -17,7 +21,7 @@ export default function Index() {
         <GymClass/>
         <GymFeature/>
         <Calendar/>
-
+        <button onClick={() => signIn('google')}>Sign in</button>
       </div>
     </Page>
   )
