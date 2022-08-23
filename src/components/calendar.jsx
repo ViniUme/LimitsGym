f78e5e41e251b9127a17c016cd05bb5dd62427b2
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-23 16:06:23 -0300

    Created calendar

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,9 +1,43 @@
-import style from '../styles/calendar.module.scss';
+import styles from '../styles/calendar.module.scss';
 
 export default function Calendar(){
     return(
-        <div>
-            
-        </div>
+        <section className={styles.calendar}>
+            <h2 className={styles.title}>Horários de Atendimento</h2>
+            <div className={styles.day_div_1}>
+                <h3 className={styles.day_title}>Segunda, Quarta e Sexta</h3>
+                <div className={styles.hours_div}>
+                    <div>
+                        05:30 À 09:00 <span className={styles.coache_name}>Gabriel</span><br/>
+                        09:00 À 12:00 <span className={styles.coache_name}>Max</span><br/>
+                        14:00 À 17:00 <span className={styles.coache_name}>Juliano</span><br/>
+                        17:00 À 20:30 <span className={styles.coache_name}>Max</span><br/>
+                        17:00 À 20:30 <span className={styles.coache_name}>Marcos</span><br/>
+                        18:00 À 20:30 <span className={styles.coache_name}>Juliano</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.day_div_2}>
+                <h3 className={styles.day_title}>Terça e Quinta</h3>
+                <div className={styles.hours_div}>
+                    <div>
+                        05:30 À 09:00 <span className={styles.coache_name}>Marcos</span><br/>
+                        09:00 À 12:00 <span className={styles.coache_name}>Max</span><br/>
+                        14:00 À 16:00 <span className={styles.coache_name}>Juliano</span><br/>
+                        16:00 À 21:30 <span className={styles.coache_name}>Marcos</span><br/>
+                        17:00 À 20:30 <span className={styles.coache_name}>Max</span><br/>
+                        18:00 À 20:30 <span className={styles.coache_name}>Juliano</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.day_div_3}>
+                <h3 className={styles.day_title}>Sábado</h3>
+                <div className={styles.hours_div}>
+                    <div>
+                        09:00 À 11:00 <span className={styles.coache_name}>Gabriel</span>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
