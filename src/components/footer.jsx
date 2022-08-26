77733efe14112f71aa08ed361495497ec4c3b1dd
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-26 14:34:17 -0300

    Create footer structure

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,8 @@
+import styles from '../styles/footer.module.scss'
+
 export default function Footer(){
     return(
-        <section>
+        <section className={styles.footer}>
             
         </section>
     )
