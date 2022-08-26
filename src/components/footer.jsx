69b6283ec19ed754cf99af4fd71580b3be106bbd
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-26 17:05:01 -0300

    Save last changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,16 @@
 import styles from '../styles/footer.module.scss'
+import Image from 'next/image';
 
 export default function Footer(){
     return(
         <section className={styles.footer}>
-            
+            <div className={styles.contact}>
+                <h3 className={styles.div_title}>Entrar em contato</h3>
+                <span><Image src="/img/local.webp" width={20} height={20} alt="" />R. Armando Sales, 489 - Centro, Osvaldo Cruz - SP, 17700-000</span>
+            </div>
+            <div className={styles.links}>
+                <h3 className={styles.div_title}>Links Rápidos</h3>
+            </div>
         </section>
     )
 }
