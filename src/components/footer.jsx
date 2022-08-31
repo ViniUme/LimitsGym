ab793d394e43e3ef465db750798db438a70c981b
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-08-31 14:05:29 -0300

    Finished footer structure and styles

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,20 @@
 import styles from '../styles/footer.module.scss'
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer(){
     return(
         <section className={styles.footer}>
             <div className={styles.contact}>
                 <h3 className={styles.div_title}>Entrar em contato</h3>
-                <span><Image src="/img/local.webp" width={20} height={20} alt="" />R. Armando Sales, 489 - Centro, Osvaldo Cruz - SP, 17700-000</span>
+                <span className={styles.div_item}><Image src="/img/local.webp" width={25} height={25} alt="" />R. Armando Sales, 489 - Osvaldo Cruz - SP</span>
+                <span className={styles.div_item}><Image src="/img/tel.webp" width={25} height={25} alt="" />(41) 99674-8137</span>
+                <Link href=""><a className={`${styles.div_item} ${styles.item_link}`}><Image src="/img/instagram.webp" width={25} height={25} alt="" />@limitsgym</a></Link>
             </div>
             <div className={styles.links}>
                 <h3 className={styles.div_title}>Links Rápidos</h3>
+                <Link href=""><a className={`${styles.div_item} ${styles.item_link}`}>{">"} Sobre Nós</a></Link>
+                <Link href=""><a className={`${styles.div_item} ${styles.item_link}`}>{">"} Contatos</a></Link>
             </div>
         </section>
     )
