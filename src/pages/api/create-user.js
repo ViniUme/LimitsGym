72d3c893b721ac08f4ee01f6282d0c0699834bb3
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-02 14:54:50 -0300

    New private api route Created a new api route to create users

---
+++ src/pages/api/create-user.js
@@ -0,0 +1,13 @@
+import Connect from '../../utils/database';
+
+export default async function handler (req,res){
+    if(req.method === "POST"){
+        const { db } = await Connect();
+        const body = req.body;
+
+        const create = await db.collection('users').insertOne(body);
+
+        console.log(create)
+        res.status(200).json({message: true})
+    }
+}
