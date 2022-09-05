c442a423e98b9ef9f952e3cf4635ba9f5156a937
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-05 16:50:44 -0300

    Insert prices route in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,6 +15,7 @@ export default function Footer(){
                 <h3 className={styles.div_title}>Links Rápidos</h3>
                 <Link href="/"><a className={`${styles.div_item} ${styles.item_link}`}>{">"} Início</a></Link>
                 <Link href="/contatos"><a className={`${styles.div_item} ${styles.item_link}`}>{">"} Contatos</a></Link>
+                <Link href="/precos"><a className={`${styles.div_item} ${styles.item_link}`}>{">"} Preços</a></Link>
             </div>
         </section>
     )
