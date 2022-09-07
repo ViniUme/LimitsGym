d6dc3be0e97b266256971379f770151a4e2257be
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-07 13:55:08 -0300

    Remove cookies function Not used functions in this route

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,8 +1,7 @@
 import Page from '../components/page';
 import { useState } from 'react';
-import { setCookie, parseCookies } from 'nookies';
 
-export default function SignRoute(props){
+export default function SignRoute(){
 
     const info = [
         ['nome', 'Nome'],
@@ -27,33 +26,12 @@ export default function SignRoute(props){
     }
     const [data, setData] = useState(json);
 
-    function Try(){
-        setCookie(null, 'USER_LOGIN', email, {
-            maxAge: 86400 * 7,
-            path: '/'
-        });
-    }
-
-    function InputValue(e){
-        setEmail(e.target.value);
-    }
-
     return(
         <Page title="Faça seu cadastro na Limits Gym" context="Tela para criação de usuário e efetuação do seu cadastro no banco de dados">
             <section>
-                <input type="text" value={email} onChange={(e) => InputValue(e)} />
-                <button onClick={() => Try()}>Show</button>
+                
+                
             </section>
         </Page>
     )
-}
-
-export async function getServerSideProps(context){
-    const res = parseCookies(context)
-
-    return {
-        props: {
-            res: res
-        }
-    }
 }
