c8a1847218c64dfc65813b478f25f059754976ae
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-07 14:25:32 -0300

    Created some form features

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -4,40 +4,64 @@ import { useState } from 'react';
 export default function SignRoute(){
 
     const info = [
-        ['nome', 'Nome'],
-        ['cid', 'Cidade'],
-        ['rua', 'Rua'],
+        ['name', 'Nome'],
+        ['city', 'Cidade'],
+        ['road', 'Rua'],
         ['email', 'E-mail'],
         ['tel', 'Telefone/Celular'],
-        ['bair', 'Bairro'],
+        ['dist', 'Bairro'],
         ['rg', 'RG'],
-        ['senha', 'Senha'],
-        ['senha', 'Comfirmar senha']
+        ['pass', 'Senha']
     ]
     const json = {
-        "nome": "",
-        "cid": "",
-        "rua": "",
+        "name": "",
+        "city": "",
+        "road": "",
         "email": "",
         "tel": "",
-        "bair": "",
+        "dist": "",
         "rg": "",
-        "senha": ""
+        "pass": ""
     }
+
     const [data, setData] = useState(json);
+    const [message, setMessage] = useState('');
+    const [verify, setVerify] = useState('');
+
+    function InputEdit(e){
+        const {id, value} = e.target;
+        setData({...data, [id]: value});
+    }
+
+    function Submit(e){
+        e.preventDefault();
+        
+        if(data.pass != verify){
+            setMessage('Suas senha estão diferentes');
+            return
+        }
+
+        console.table(data);
+    }
 
     return(
         <Page title="Faça seu cadastro na Limits Gym" context="Tela para criação de usuário e efetuação do seu cadastro no banco de dados">
             <section>
-                <form>
+                <form onSubmit={(e) => Submit(e)}>
                     {info && info.map((item, key) => {
                         return(
                             <div key={key}>
                                 <label htmlFor={info[0]}>{item[1]}</label>
-                                <input type="text" id={item[0]} value={eval(`data.${item[0]}`)} />
+                                <input type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' />
                             </div>
                         )
                     })}
+                    <div>
+                        <label htmlFor='verify'>Confirmar Senha</label>
+                        <input type="text" id='verify' value={verify} onChange={(e) => setVerify(e.target.value)} autoComplete='off' />
+                    </div>
+                    <span>{message}</span>
+                    <input type='submit' />
                 </form>
             </section>
         </Page>
