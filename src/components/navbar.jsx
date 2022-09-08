dfb4824a134c4694f8cc0f4d1894541c5671be03
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-08 16:42:53 -0300

    Insert links Navbar Insert login and sign on links on Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import style from '../styles/navbar.module.scss';
 import { useState } from 'react';
 
-export default function Navbar({type}){
+export default function Navbar(){
 
     const [activate, setActivate] = useState(style.navbar);
 
@@ -15,27 +15,25 @@ export default function Navbar({type}){
         }
     }
 
-    function ChoseColor(){
-      if(type == 1){
-        return "linear-gradient(180deg, #101010df 50%, #00000000 100%)"
-      }
-      else{
-        return "#101010"
-      }
-    }
-
     return(
-        <nav className={activate} style={{background: ChoseColor()}}>
+        <nav className={activate}>
 
-        <div className={style.mobile_menu} onClick={() => SwitchNavbar()}>
-          <div className={style.menu_line_1}></div>
-          <div className={style.menu_line_2}></div>
-          <div className={style.menu_line_3}></div>
-        </div>
+          <div className={style.mobile_menu} onClick={() => SwitchNavbar()}>
+            <div className={style.menu_line_1}></div>
+            <div className={style.menu_line_2}></div>
+            <div className={style.menu_line_3}></div>
+          </div>
 
-        <Link href="/"><a className={style.nav_link_main}>início</a></Link>
-        <Link href="/contatos"><a className={style.nav_link}>contatos</a></Link>
-        <Link href="/precos"><a className={style.nav_link}>preços</a></Link>
+          <div>
+            <Link href="/"><a className={style.nav_link_main}>início</a></Link>
+            <Link href="/contatos"><a className={style.nav_link}>contatos</a></Link>
+            <Link href="/precos"><a className={style.nav_link}>preços</a></Link>
+          </div>
+
+          <div className={style.sign_on_in}>
+          <Link href="/entrar"><a className={style.nav_link}>entrar</a></Link>
+            <Link href="/cadastro"><a className={style.nav_link}>cadastrar</a></Link>
+          </div>
 
       </nav>
     )
