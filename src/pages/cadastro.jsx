8beaa437204d676df515e4d1e59af74ffac65549
ViniUme <viniciusumetsu210@gmail.com>
2022-09-10 15:56:16 -0300

    Import Loading component

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,9 +1,11 @@
 import Page from '../components/page';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import Loading from '../components/loading';
 import styles from '../styles/cadastro.module.scss';
 import { CreateUser, VerifyUser } from '../utils/functions';
 import { useState } from 'react';
+import { imageConfigDefault } from 'next/dist/server/image-config';
 
 export default function SignRoute(){
 
@@ -29,7 +31,7 @@ export default function SignRoute(){
     }
 
     const [data, setData] = useState(json);
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('loading');
     const [verify, setVerify] = useState('');
 
     function InputEdit(e){
@@ -51,43 +53,54 @@ export default function SignRoute(){
         const verify_user = await VerifyUser(client);
 
         if(verify_user === true){
-            setMessage('useário já existente');
+            setMessage('usuário já existente');
         }
         else{
-            await CreateUser(client);
+            const response = await CreateUser(client);
+            
+            if(response === true){
+
+            }
         }
+    }
+    if(message != 'loading'){
+        return(
+            <Page title="Faça seu cadastro na Limits Gym" context="Tela para criação de usuário e efetuação do seu cadastro no banco de dados">
+                <Navbar/>
+
+                <h1 className={styles.header}>preencha o formulário com seus dados para se cadastrar na acamedia limits gym</h1>
+
+                <form className={styles.sign_on} onSubmit={(e) => Submit(e)}>
+
+                    {info && info.map((item, key) => {
+                        return(
+                            <div className={styles.div_input} key={key}>
+                                <input className={styles.input} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' />
+                                <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
+                                <span className={styles.line} />
+                            </div>
+                        )
+                    })}
+
+                    <div className={styles.div_input}>
+                        <input className={styles.input} placeholder=" " type="text" id='verify' value={verify} onChange={(e) => setVerify(e.target.value)} autoComplete='off' />
+                        <label className={styles.label} htmlFor='verify'>Confirmar Senha</label>
+                        <span className={styles.line} />
+                    </div>
+
+                    <span className={styles.message}>{message}</span>
+                    <input className={styles.button} type='submit' />
+                    
+                </form>
+
+                <Footer/>
+            </Page>
+        )
     }
 
     return(
-        <Page title="Faça seu cadastro na Limits Gym" context="Tela para criação de usuário e efetuação do seu cadastro no banco de dados">
-            <Navbar/>
-
-            <h1 className={styles.header}>preencha o formulário com seus dados para se cadastrar na acamedia limits gym</h1>
-
-            <form className={styles.sign_on} onSubmit={(e) => Submit(e)}>
-
-                {info && info.map((item, key) => {
-                    return(
-                        <div className={styles.div_input} key={key}>
-                            <input className={styles.input} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' />
-                            <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                            <span className={styles.line} />
-                        </div>
-                    )
-                })}
-
-                <div className={styles.div_input}>
-                    <input className={styles.input} placeholder=" " type="text" id='verify' value={verify} onChange={(e) => setVerify(e.target.value)} autoComplete='off' />
-                    <label className={styles.label} htmlFor='verify'>Confirmar Senha</label>
-                    <span className={styles.line} />
-                </div>
-
-                <span className={styles.message}>{message}</span>
-                <input className={styles.button} type='submit' />
-                
-            </form>
-
-            <Footer/>
-        </Page>
+        <>
+            {Loading()}
+        </>
     )
 }
