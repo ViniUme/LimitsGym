c245f0ee9dc399d50b59bca830c92c26ba9c04f7
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-12 14:34:59 -0300

    Finalize login route styles Finalize login route styles, and start backend functions

--- src/pages/entrar.jsx
+++ src/pages/entrar.jsx
@@ -1,3 +1,45 @@
-export default function Lognin(){
-    
+import Page from '../components/page';
+import { useState } from 'react';
+import styles from '../styles/entrar.module.scss'
+
+export default function Login(){
+
+    const json = {
+        email: '',
+        pass: ''
+    }
+    const [data, setData] = useState(json);
+    const [message, setMessage] = useState('');
+
+    function InputEdit(e){
+        const {id, value} = e.target;
+        setData({...data, [id]: value});
+    }
+
+    function Submit(e){
+        e.preventDefault();
+        console.log(data);
+    }
+
+    return(
+        <Page title='Entre com sua conta na Limits Gym' description='Tela para acessar a conta do cliente da academia'>
+            <form className={styles.login} onSubmit={(e) => Submit(e)}>
+                <h1 className={styles.title}>entre com seu e-mail</h1>
+                <div className={styles.input_div}>
+                    <input className={styles.input} placeholder=' ' id='email' type='text' value={data.email} onChange={(e) => InputEdit(e)} autoComplete='off' />
+                    <label className={styles.label} htmlFor='email'>E-mail</label>
+                    <span className={styles.line} />
+                </div>
+                <div className={styles.input_div}>
+                    <input className={styles.input} placeholder=' ' id='pass' type='text' value={data.pass} onChange={(e) => InputEdit(e)} autoComplete='off' />
+                    <label className={styles.label} htmlFor='pass'>Senha</label>
+                    <span className={styles.line} />
+                </div>
+
+                <span className={styles.message}>{message}</span>
+
+                <input className={styles.button} type='submit' value="entrar" />
+            </form>
+        </Page>
+    )
 }
