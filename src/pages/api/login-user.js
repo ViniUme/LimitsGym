8e94f1e4365c2000da7cd9ac4aa37a386a50bf1c
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-12 14:43:22 -0300

    New api route, login-client

---
+++ src/pages/api/login-user.js
@@ -0,0 +1,17 @@
+import Connect from "../../utils/database";
+
+export default async function handler(req,res){
+    if(req.method == 'POST'){
+        const {db} = Connect();
+        const body = req.body;
+
+        const response = await db.collection('clients').findOne({email: body.email, pass: body.pass});
+
+        if(response != null){
+            res.status(200).json({message: true});
+        }
+        else{
+            res.status(200).json({message: false});
+        }
+    }
+}
