a0a4d438da069e5d6be0db7083d5a4b33d883343
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-14 16:48:27 -0300

    Set cookies identifier on prices route

--- src/pages/precos.jsx
+++ src/pages/precos.jsx
@@ -1,9 +1,20 @@
 import Prices from '../components/prices'
 import Page from '../components/page';
+import { parseCookies } from 'nookies';
 
-export default function Precos(){
+export function getServerSideProps(context){
+    const cookies = parseCookies(context);
+
+    return {
+        props: {
+            cookies: cookies
+        }
+    }
+}
+
+export default function Precos(props){
     return(
-        <Page title="Preços dos Planos" context="Página que mostra o preço dos planos">
+        <Page title="Preços dos Planos" context="Página que mostra o preço dos planos" cookies={props.cookies}>
             <Prices/>
         </Page>
     )
