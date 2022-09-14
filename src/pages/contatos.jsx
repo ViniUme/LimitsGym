639e4897a5b1a98ca9106aac1706b87361e01b2b
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-14 16:48:18 -0300

    Set cookies identifier on Contacts route

--- src/pages/contatos.jsx
+++ src/pages/contatos.jsx
@@ -1,11 +1,22 @@
 import Page from '../components/page';
 import Contacts from '../components/contact';
 import Coaches from '../components/coaches';
+import { parseCookies } from 'nookies';
 import styles from '../styles/contatos.module.scss';
 
-export default function Contatos(){
+export function getServerSideProps(context){
+    const cookies = parseCookies(context);
+
+    return {
+        props: {
+            cookies: cookies
+        }
+    }
+}
+
+export default function Contatos(props){
     return(
-        <Page title="Contatos Limits Gym" description="Página com todos contatos da Limits Gym">
+        <Page title="Contatos Limits Gym" description="Página com todos contatos da Limits Gym" cookies={props.cookies}>
             <section className={styles.contatos}>
                 <Contacts/>
                 <Coaches/>
