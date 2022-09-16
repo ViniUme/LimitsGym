796b57c07d2222f70a1b1f3810aa80ac455fa11f
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-16 13:56:36 -0300

    Created password input mask

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Page from '../components/page';
 import Loading from '../components/loading';
 import { mask } from 'remask';
@@ -44,15 +46,22 @@ export default function SignRoute(props){
         tel: ['(99) 99999-9999', '(99) 9999-9999'],
         rg: '99.999.999-9'
     }
+    const pass_visible = {
+        pass: {
+            type: 'password',
+            className: styles.eye_svg,
+        },
+        verify: {
+            type: 'password',
+            className: styles.eye_svg
+        }
+    }
 
     const [data, setData] = useState(json);
     const [message, setMessage] = useState('');
     const [verify, setVerify] = useState('');
     const [pattern, setPattern] = useState(input_mask);
-    const [visible, setVisible] = useState({
-        pass: 'password',
-        verify: 'password'
-    });
+    const [visible, setVisible] = useState(pass_visible);
 
 
     function InputEdit(e){
@@ -73,11 +82,25 @@ export default function SignRoute(props){
     }
 
     function HiddenPassword(id){
-        if(visible[id] == 'password'){
-            setVisible({...visible, [id]: 'text'});
+        console.log(id)
+        if(visible[id].type == 'password'){
+            setVisible({
+                ...visible,
+                [id]: {
+                    type: 'text',
+                    className: `${styles.eye_svg} ${styles.eye_svg_red}`,
+                }
+            });
         }
         else{
-            setVisible({...visible, [id]: 'password'})
+            setVisible({
+                ...visible,
+                [id]: {
+                    type: 'password',
+                    className: styles.eye_svg
+                }
+            })
+
         }
     }
 
@@ -165,12 +188,20 @@ export default function SignRoute(props){
                             )
                         }
                         if(item[0] == 'pass'){
+                            console.log(visible.pass)
                             return(
                                 <div className={styles.div_input} key={key}>
-                                    <button className={styles.eye_pass} onClick={() => HiddenPassword('pass')}></button>
-                                    <input className={styles.input} placeholder=" " type={visible.pass} id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' />
+                                    <button className={styles.eye_pass} onClick={() => HiddenPassword('pass')}>
+                                        <svg className={visible.pass.className} version="1.0" xmlns="http://www.w3.org/2000/svg" width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+                                            <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" stroke="none">
+                                                <path d="M2385 3904 c-320 -35 -568 -96 -850 -209 -477 -192 -984 -531 -1387 -926 -144 -141 -162 -173 -139 -251 21 -74 327 -359 616 -574 528 -392 1066 -630 1612 -711 162 -24 484 -24 646 0 468 70 914 247 1372 545 259 169 507 367 717 573 144 141 162 173 139 252 -21 73 -327 358 -616 573 -525 390 -1070 631 -1605 709 -103 15 -426 27 -505 19z m318 -305 c518 -77 907 -522 907 -1039 0 -571 -479 -1050 -1050 -1050 -628 0 -1131 578 -1039 1193 86 574 621 980 1182 896z m-1267 -294 c-99 -151 -161 -302 -202 -495 -26 -119 -26 -381 0 -500 41 -194 102 -343 203 -497 16 -25 6 -21 -109 39 -134 70 -329 191 -462 286 -182 132 -496 398 -496 422 0 14 231 218 356 315 125 98 303 221 419 290 73 44 295 164 304 165 2 0 -4 -11 -13 -25z m2611 -185 c158 -101 252 -169 402 -288 130 -104 301 -259 301 -272 0 -14 -231 -218 -356 -315 -204 -159 -451 -319 -643 -415 -82 -41 -84 -41 -67 -16 99 152 161 303 202 496 26 119 26 381 0 500 -41 194 -102 343 -203 497 -16 25 -6 21 109 -39 70 -37 185 -103 255 -148z"/>
+                                                <path d="M2465 3304 c-11 -2 -45 -9 -75 -15 -217 -44 -429 -224 -522 -444 -41 -97 -53 -162 -53 -285 0 -123 12 -188 53 -285 93 -219 290 -387 522 -446 71 -18 269 -18 340 0 232 59 429 227 522 446 41 97 53 162 53 285 0 65 -6 139 -14 170 -68 269 -277 482 -546 557 -51 14 -243 26 -280 17z m176 -304 c235 -45 399 -275 361 -506 -32 -196 -180 -344 -376 -376 -289 -48 -556 219 -508 508 42 252 277 420 523 374z"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+                                    <input className={`${styles.input} ${styles.input_pass}`} placeholder=" " type={visible.pass.type} id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' />
                                     <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                    <span className={styles.line} />
+                                    <span className={`${styles.line} ${styles.line_pass}`} />
                                 </div>
                             )
                         }
@@ -184,10 +215,17 @@ export default function SignRoute(props){
                     })}
 
                     <div className={styles.div_input}>
-                        <button className={styles.eye_pass} onClick={() => HiddenPassword('verify')}></button>
-                        <input className={styles.input} placeholder=" " type={visible.verify} id='verify' value={verify} onChange={(e) => setVerify(e.target.value)} autoComplete='off' />
+                        <button className={styles.eye_pass} onClick={() => HiddenPassword('verify')}>
+                            <svg className={visible.verify.className} version="1.0" xmlns="http://www.w3.org/2000/svg" width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+                                <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" stroke="none">
+                                    <path d="M2385 3904 c-320 -35 -568 -96 -850 -209 -477 -192 -984 -531 -1387 -926 -144 -141 -162 -173 -139 -251 21 -74 327 -359 616 -574 528 -392 1066 -630 1612 -711 162 -24 484 -24 646 0 468 70 914 247 1372 545 259 169 507 367 717 573 144 141 162 173 139 252 -21 73 -327 358 -616 573 -525 390 -1070 631 -1605 709 -103 15 -426 27 -505 19z m318 -305 c518 -77 907 -522 907 -1039 0 -571 -479 -1050 -1050 -1050 -628 0 -1131 578 -1039 1193 86 574 621 980 1182 896z m-1267 -294 c-99 -151 -161 -302 -202 -495 -26 -119 -26 -381 0 -500 41 -194 102 -343 203 -497 16 -25 6 -21 -109 39 -134 70 -329 191 -462 286 -182 132 -496 398 -496 422 0 14 231 218 356 315 125 98 303 221 419 290 73 44 295 164 304 165 2 0 -4 -11 -13 -25z m2611 -185 c158 -101 252 -169 402 -288 130 -104 301 -259 301 -272 0 -14 -231 -218 -356 -315 -204 -159 -451 -319 -643 -415 -82 -41 -84 -41 -67 -16 99 152 161 303 202 496 26 119 26 381 0 500 -41 194 -102 343 -203 497 -16 25 -6 21 109 -39 70 -37 185 -103 255 -148z"/>
+                                    <path d="M2465 3304 c-11 -2 -45 -9 -75 -15 -217 -44 -429 -224 -522 -444 -41 -97 -53 -162 -53 -285 0 -123 12 -188 53 -285 93 -219 290 -387 522 -446 71 -18 269 -18 340 0 232 59 429 227 522 446 41 97 53 162 53 285 0 65 -6 139 -14 170 -68 269 -277 482 -546 557 -51 14 -243 26 -280 17z m176 -304 c235 -45 399 -275 361 -506 -32 -196 -180 -344 -376 -376 -289 -48 -556 219 -508 508 42 252 277 420 523 374z"/>
+                                </g>
+                            </svg>
+                        </button>
+                        <input className={`${styles.input} ${styles.input_pass}`} placeholder=" " type={visible.verify.type} id='verify' value={verify} onChange={(e) => setVerify(e.target.value)} autoComplete='off' />
                         <label className={styles.label} htmlFor='verify'>Confirmar Senha</label>
-                        <span className={styles.line} />
+                        <span className={`${styles.line} ${styles.line_pass}`} />
                     </div>
 
                     <span className={styles.message}>{message}</span>
