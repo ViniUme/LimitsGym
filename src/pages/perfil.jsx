1762bae46f1d185d607d7c01eec3218b566f5a7c
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-20 14:51:30 -0300

    New route, profile Created new route, just structure

---
+++ src/pages/perfil.jsx
@@ -0,0 +1,11 @@
+import Page from '../components/page';
+
+export default function Profile(){
+    return(
+        <Page title='Perfil do usuário' description='Página onde mostra todos os dados sobre o usuário'>
+            <section>
+                
+            </section>
+        </Page>
+    )
+}
