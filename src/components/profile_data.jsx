2158c60c05f4a99716ebd0fc9fd0d30244a4ff10
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-21 17:09:48 -0300

    New component, profile_data

---
+++ src/components/profile_data.jsx
@@ -0,0 +1,78 @@
+import EditForm from './edit_form';
+import { useState } from 'react';
+import styles from '../styles/profile.module.scss';
+
+export default function ProfileData({data}){
+    
+    const info = [['city', 'Cidade'], ['road', 'Rua'], ['num', 'Número'], ['email', 'Email'], ['dist', 'Bairro'], ['tel', 'Telefone'], ['rg', 'RG'], ['date', 'Data de criação']]
+
+    const [display, setDisplay] = useState('read');
+
+    function GoEdit(){
+        if(display == 'read'){
+            setDisplay('write');
+        }
+    }
+
+    if(display == 'read'){
+        return( 
+            <section className={styles.profile}>
+                <h1 className={styles.name}>{data.name}</h1>
+                <div className={styles.profile_item}>
+                    <h1 className={styles.item_title}>meus dados</h1>
+                    {info && info.map((item, key) => {
+                        if(item[0] == 'num'){
+                            return(
+                                <div className={styles.div_input} key={key}>
+                                    <input className={`${styles.input} ${styles.input_read}`} placeholder=" " type="text" value={eval(`data.${item[0]}`)} readOnly />
+                                    <label className={styles.label} >{item[1]}</label>
+                                    <span className={styles.line} />
+                                </div>
+                            )    
+                        }
+                        if(item[0] == 'rg'){
+                            return(
+                                <div className={styles.div_input} key={key}>
+                                    <input className={styles.input} placeholder=" " type="text" value={eval(`data.${item[0]}`)} readOnly />
+                                    <label className={styles.label} >{item[1]}</label>
+                                    <span className={styles.line} />
+                                </div>
+                            )    
+                        }
+                        if(item[0] == 'tel'){
+                            return(
+                                <div className={styles.div_input} key={key}>
+                                    <input className={styles.input} placeholder=" " type="text" value={eval(`data.${item[0]}`)} readOnly />
+                                    <label className={styles.label} >{item[1]}</label>
+                                    <span className={styles.line} />
+                                </div>
+                            )
+                        }
+                        if(item[0] == 'pass'){
+                            return(
+                                <div className={styles.div_input} key={key}>
+                                    <input className={`${styles.input}`} placeholder=" " type='password' value={eval(`data.${item[0]}`)} readOnly />
+                                    <label className={styles.label} >{item[1]}</label>
+                                    <span className={`${styles.line}`} />
+                                </div>
+                            )
+                        }
+                        return(
+                            <div className={styles.div_input} key={key}>
+                                <input className={styles.input} placeholder=" " type="text" value={eval(`data.${item[0]}`)} readOnly />
+                                <label className={styles.label} >{item[1]}</label>
+                                <span className={styles.line} />
+                            </div>
+                        )
+                    })}
+                    <button className={styles.edit_button} onClick={() => GoEdit()}>editar informações</button>
+                </div>
+            </section>
+        )
+    }
+    if(display == 'write'){
+        return(
+            <EditForm user={data} />
+        )
+    }
+}
