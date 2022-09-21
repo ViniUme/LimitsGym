2f3090f972bc9b91e3f5a1fef9d2925aefdb7956
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-21 17:09:20 -0300

    Finished Route perfil Finished Route perfil, but building profile_data & edit_form components

--- src/pages/perfil.jsx
+++ src/pages/perfil.jsx
@@ -1,86 +1,31 @@
 import Page from '../components/page';
 import { VerifyUser } from '../utils/functions';
 import { parseCookies } from 'nookies';
+import ProfileData from '../components/profile_data';
 import EditForm from '../components/edit_form';
+import { mask } from 'remask';
+import { Patterns } from '../utils/variables';
 import { useState } from 'react';
-import styles from '../styles/profile.module.scss';
 
 export async function getServerSideProps(context){
+    const pattern = Patterns();
     const cookies = parseCookies(context);
-    const user_data = await VerifyUser({email: cookies.USER_LOGIN});
+    const response = await VerifyUser({email: cookies.USER_LOGIN});
+    const user_data = {...response.user, tel: mask(response.user.tel, pattern.tel), rg: mask(response.user.rg, pattern.rg), pass: null};
 
     return{
         props: {
             cookies: cookies,
-            data: user_data.user
+            data: user_data
         }
     }
 }
 
 export default function Profile({cookies, data}){
-    
-    const [edit, setEdit] = useState(null);
-    const info = [['city', 'Cidade'], ['road', 'Rua'], ['num', 'Número'], ['email', 'Email'], ['dist', 'Bairro'], ['tel', 'Telefone'], ['rg', 'RG'], ['date', 'Data de criação']]
-
-    function GetEdit(){
-        setEdit(<EditForm />);
-    }
 
     return(
         <Page title='Perfil do usuário' description='Página onde mostra todos os dados sobre o usuário' cookies={cookies}>
-            {edit}
-            <section className={styles.profile}>
-                <h1 className={styles.name}>{data.name}</h1>
-                <div className={styles.profile_item}>
-                    <h1 className={styles.item_title}>meus dados</h1>
-                    {info && info.map((item, key) => {
-                        if(item[0] == 'num'){
-                            return(
-                                <div className={styles.div_input} key={key}>
-                                    <input className={`${styles.input} ${styles.input_read}`} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' maxLength={5} inputMode='numeric' readOnly />
-                                    <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                    <span className={styles.line} />
-                                </div>
-                            )    
-                        }
-                        if(item[0] == 'rg'){
-                            return(
-                                <div className={styles.div_input} key={key}>
-                                    <input className={styles.input} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' inputMode='numeric' readOnly />
-                                    <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                    <span className={styles.line} />
-                                </div>
-                            )    
-                        }
-                        if(item[0] == 'tel'){
-                            return(
-                                <div className={styles.div_input} key={key}>
-                                    <input className={styles.input} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' readOnly />
-                                    <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                    <span className={styles.line} />
-                                </div>
-                            )
-                        }
-                        if(item[0] == 'pass'){
-                            return(
-                                <div className={styles.div_input} key={key}>
-                                    <input className={`${styles.input}`} placeholder=" " type='password' id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' readOnly />
-                                    <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                    <span className={`${styles.line}`} />
-                                </div>
-                            )
-                        }
-                        return(
-                            <div className={styles.div_input} key={key}>
-                                <input className={styles.input} placeholder=" " type="text" id={item[0]} value={eval(`data.${item[0]}`)} onChange={(e) => InputEdit(e)} autoComplete='off' readOnly />
-                                <label className={styles.label} htmlFor={item[0]}>{item[1]}</label>
-                                <span className={styles.line} />
-                            </div>
-                        )
-                    })}
-                    <button className={styles.edit_button} onClick={() => GetEdit()}>editar informações</button>
-                </div>
-            </section>
+            <ProfileData data={data}/>
         </Page>
     )
 }
