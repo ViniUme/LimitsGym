7870b54a0cae73cf5860d86217ce7d533743d819
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-22 16:13:13 -0300

    New feature Profile Indentify base url and use to get server side props

--- src/pages/perfil.jsx
+++ src/pages/perfil.jsx
@@ -2,15 +2,15 @@ import Page from '../components/page';
 import { VerifyUser } from '../utils/functions';
 import { parseCookies } from 'nookies';
 import ProfileData from '../components/profile_data';
-import EditForm from '../components/edit_form';
 import { mask } from 'remask';
 import { Patterns } from '../utils/variables';
-import { useState } from 'react';
 
 export async function getServerSideProps(context){
     const pattern = Patterns();
     const cookies = parseCookies(context);
-    const response = await VerifyUser({email: cookies.USER_LOGIN});
+
+    const url = context.req.rawHeaders[1]
+    const response = await VerifyUser(cookies.USER_LOGIN, url);
     const user_data = {...response.user, tel: mask(response.user.tel, pattern.tel), rg: mask(response.user.rg, pattern.rg), pass: null};
 
     return{
