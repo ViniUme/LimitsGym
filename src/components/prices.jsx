aacce918e89e4a677522cc1dea570d4abc005b83
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-28 14:27:42 -0300

    Modified pricing styles

--- src/components/prices.jsx
+++ src/components/prices.jsx
@@ -7,13 +7,14 @@ export default function Prices({user}){
 
     const [gym_plan, setGymPlan] = useState(null);
 
-    function Buy(price, time, plan, type){
+    function Buy(price, total, time, plan, type){
         if(user.USER_LOGIN == (undefined || null)){
             window.location.href = '/entrar';
         }
         else{
             setGymPlan({
                 price: price,
+                total: total,
                 time: time,
                 plan: plan,
                 type: type
@@ -34,7 +35,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>5,00<span className={styles.month}>/dia</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por quinzena é R$ 75,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('5', 'dia', 'Quinzenal', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('5', 'por quinzena é R$ 75,00', 'dia', 'Quinzenal', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -43,7 +44,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>7,00<span className={styles.month}>/dia</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por semana é R$ 49,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('7', 'dia', 'Semanal', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('7', 'por semana é R$ 49,00', 'dia', 'Semanal', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -52,7 +53,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>20,00<span className={styles.month}>/dia</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por dia é R$ 20,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('20', 'dia', 'Diário', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('20', 'por dia é R$ 20,00', 'dia', 'Diário', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -61,7 +62,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>120,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por mês é R$ 120,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('120', 'mês', 'Mensal', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('120', 'por mês é R$ 120,00', 'mês', 'Mensal', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -70,7 +71,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>115,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por trimestre é R$ 345,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('115', 'mês', 'Trimestral', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('115', 'por trimestre é R$ 345,00', 'mês', 'Trimestral', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -79,7 +80,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>110,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por semestre é R$ 660,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('110', 'mês', 'Semestral', 'Individual')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('110', 'por semestre é R$ 660,00', 'mês', 'Semestral', 'Individual')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -87,8 +88,8 @@ export default function Prices({user}){
                         <h2 className={styles.price_string}>
                             <span className={styles.dollar_sign}>R$ </span>105,00<span className={styles.month}>/mês</span>
                         </h2>
-                        <h3 className={styles.text}>Total pago por ano é R$ 1260,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('105', 'mês', 'Anual', 'Individual')}>assinar</button>
+                        <h3 className={styles.text}>Total pago por ano é R$ 1.260,00.</h3>
+                        <button className={styles.button} onClick={() => Buy('105', 'por Ano é R$ 1.260,00', 'mês', 'Anual', 'Individual')}>assinar</button>
                     </div>
 
                 </div>
@@ -96,13 +97,13 @@ export default function Prices({user}){
                 <h1 className={styles.title}>Planos familiares</h1>
                 <div className={styles.different_prices}>
 
-                <div className={styles.price_div}>
+                    <div className={styles.price_div}>
                         <h1 className={styles.price_title}>Plano Mensal</h1>
                         <h2 className={styles.price_string}>
                             <span className={styles.dollar_sign}>R$ </span>115,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por mês é R$ 115,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('115', 'mês', 'Mensal', 'Familiar')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('115', 'por mês é R$ 115,00', 'mês', 'Mensal', 'Familiar')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -111,7 +112,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>110,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por trimestre é R$ 330,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('110', 'mês', 'Trimestral', 'Familiar')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('110', 'por trimestre é R$ 330,00', 'mês', 'Trimestral', 'Familiar')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -120,7 +121,7 @@ export default function Prices({user}){
                             <span className={styles.dollar_sign}>R$ </span>105,00<span className={styles.month}>/mês</span>
                         </h2>
                         <h3 className={styles.text}>Total pago por semestre é R$ 630,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('105', 'mês', 'Semestral', 'Familiar')}>assinar</button>
+                        <button className={styles.button} onClick={() => Buy('105', 'por semestre é R$ 630,00', 'mês', 'Semestral', 'Familiar')}>assinar</button>
                     </div>
 
                     <div className={styles.price_div}>
@@ -128,8 +129,8 @@ export default function Prices({user}){
                         <h2 className={styles.price_string}>
                             <span className={styles.dollar_sign}>R$ </span>100,00<span className={styles.month}>/mês</span>
                         </h2>
-                        <h3 className={styles.text}>Total pago por ano é R$ 120,00.</h3>
-                        <button className={styles.button} onClick={() => Buy('100', 'mês', 'Anual', 'Familiar')}>assinar</button>
+                        <h3 className={styles.text}>Total pago por ano é R$ 1.200,00.</h3>
+                        <button className={styles.button} onClick={() => Buy('100', 'por Ano é R$ 1.200,00', 'mês', 'Anual', 'Familiar')}>assinar</button>
                     </div>
 
                 </div>
@@ -142,23 +143,17 @@ export default function Prices({user}){
             if((gym_plan.plan == 'Anual') && (gym_plan.type == 'Individual')){
                 benefits_var.push('Conan;');
             }
-            if((gym_plan.plan == 'Anual') && (gym_plan.type == 'Familiar')){
-                benefits_var.push('Barbaros Lendários;');
-            }
             if((gym_plan.price == '20') || (gym_plan.price == '7') || (gym_plan.price == '5')){
-                benefits_var.push('Mais acessível para o bolso;', 'Ter apenas uma experiência;');
+                benefits_var.push('Apenas experimentar;', 'Mais acessível para o bolso;');
             }
             if((gym_plan.plan == 'Mensal') || (gym_plan.plan == 'Trimestral') || (gym_plan.plan == 'Semestral') || (gym_plan.plan == 'Anual')){
-                benefits_var.push('Desenvolva uma rótina de treino;', 'Mais barato à longo prazo;', 'Perfeito para quem gosta de malhar;');
+                benefits_var.push('Mais barato;', 'Rótina saudável;', 'Perceberá resultados;');
             }
             if((gym_plan.plan == 'Semestral') || (gym_plan.plan == 'Anual')){
-                benefits_var.push('Quanto mais tempo treinando, melhor;');
+                benefits_var.push('Transformação do corpo;');
             }
             if(gym_plan.type == 'Familiar'){
-                benefits_var.push('Para a família é mais barato;');
-            }
-            if((gym_plan.plan == 'Anual') && (gym_plan.type == 'Familiar')){
-                benefits_var.push('O plano mais custo-benefício;');
+                benefits_var.push('Mais acessível para toda família;');
             }
 
             return benefits_var
@@ -213,12 +208,14 @@ export default function Prices({user}){
 
         return(
             <section className={styles.buy}>
-                <h1 className={styles.title}>plano {gym_plan.plan} {gym_plan.type}</h1>
+                <h1 className={styles.title}>{gym_plan.plan} {gym_plan.type}</h1>
                 <h2 className={styles.price_string}>
                     <span className={styles.dollar_sign}>R$ </span>{gym_plan.price},00<span className={styles.month}>/{gym_plan.time}</span>
                 </h2>
+
+                <h3 className={styles.text}>Total pago {gym_plan.total}.</h3>
                 
-                <h3 className={styles.benefits_title}>Benefícios deste plano</h3>
+                <h3 className={styles.benefits_title}>Benefícios</h3>
                 <div className={styles.benefits}>
                     {List().map((item, key) => {
                         return(
