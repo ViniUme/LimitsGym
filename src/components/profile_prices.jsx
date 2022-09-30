72f63a8fb401fa75be7f2cc2b76fd01335d2ddde
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-09-30 13:33:03 -0300

    New modal created

--- src/components/profile_prices.jsx
+++ src/components/profile_prices.jsx
@@ -1,19 +1,27 @@
+import Modal from 'react-modal';
 import { useState } from 'react';
-import { CancelPlan } from '../components/pop_ups';
 import styles from '../styles/profile_data.module.scss';
+
+Modal.setAppElement('#__next');
 
 export default function ProfilePrices({data}){
 
     const date_start = new Date(data.plan.date_start);
     const date_end = new Date(data.plan.date_end);
     const date_now = new Date();
+    const [modalIsOpoen, setIsOpen] = useState(false);
     const [render, setRender] = useState({
-        "days_message": {
-            "today": <span className={styles.color_red}>Você tem apenas hoje para ir à Academia</span>,
-            "finished": <>Parece que seu plano <span className={styles.color_red}>já acabou</span>, renove seu plano para continuar na limits</>
-        },
-        "pop_up": null
+        today: <span className={styles.color_red}>Você tem apenas hoje para ir à Academia</span>,
+        finished: <>Parece que seu plano <span className={styles.color_red}>já acabou</span>, renove seu plano para continuar na limits</> 
     })
+
+    function OpenModal(){
+        setIsOpen(true);
+    }
+
+    function CloseModal(){
+        setIsOpen(false);
+    }
 
     function CheckTime(date){
         if((date === 'Quinzenal') || (date === 'Semanal') || (date === 'Diário')){
@@ -26,17 +34,21 @@ export default function ProfilePrices({data}){
 
     function DaysMissing(){
         
-        console.log(date_end.getDay(), date_start.getDay())
-        console.log(date_end.getDay() == date_start.getDay())
         if(date_end.getDay() === date_now.getDay()){
-            return render.days_message.today
+            return render.today
         }
         if(date_now > date_end){
-            return render.days_message.finished
+            return render.finished
         }
         
         const date_difernce = Math.round(Math.abs(date_now - date_end) / (1000 * 60 * 60 * 24));
-        return <>Você ainda tem <span className={styles.color_red}>{date_difernce} dias</span> para ir à Academia</>
+        
+        if(date_difernce == 1){
+            return <>Você ainda tem <span className={styles.color_red}>até amanhã</span> para ir à Academia</>
+        }
+        else{
+            return <>Você ainda tem <span className={styles.color_red}>{date_difernce} dias</span> para ir à Academia</>
+        }
     }
 
     function ReturnButton(){
@@ -46,7 +58,7 @@ export default function ProfilePrices({data}){
     }
 
     function CancelButton(){
-        
+        setIsOpen(true);
     }
     
     if(data.plan != undefined){
@@ -59,7 +71,7 @@ export default function ProfilePrices({data}){
                     <h3 className={`${styles.grid_item} ${styles.left}`}>Tipo & Tempo:</h3>
                     <h3 className={`${styles.grid_item} ${styles.right}`}>{data.plan.time} {data.plan.type}</h3>
                     <h3 className={`${styles.grid_item} ${styles.left}`}>Início do plano:</h3>
-                    <h3 className={`${styles.grid_item} ${styles.right}`}>{date_end.toLocaleDateString()}</h3>
+                    <h3 className={`${styles.grid_item} ${styles.right}`}>{date_start.toLocaleDateString()}</h3>
                     <h3 className={`${styles.grid_item} ${styles.left}`}>Termino do plano:</h3>
                     <h3 className={`${styles.grid_item} ${styles.right}`}>{date_end.toLocaleDateString()}</h3>
                 </div>
@@ -71,7 +83,20 @@ export default function ProfilePrices({data}){
                     <button className={`${styles.button} ${styles.trade}`}>trocar</button>
                     {ReturnButton()}
                 </div>
-                {render.pop_up}
+
+                <Modal isOpen={modalIsOpoen} onRequestClose={CloseModal} className={styles.modal}>
+                    <section className={styles.pop_up}>
+                        <h1 className={styles.title}>cancelar plano ?</h1>
+                        <h2 className={styles.sub}>
+                            Você quer cancelar o seu plano atual da academia ? E perder dia de treino, os benefícios à saúde e o shape ? Você tem certeza disso ?
+                        </h2>
+                        <div className={styles.div_buttons}>
+                            <button className={`${styles.button} ${styles.false}`} onClick={() => CloseModal()}>não, voltar atrás</button>
+                            <button className={`${styles.button} ${styles.true}`} onClick={() => window.location.href = '/planos'}>sim, certeza</button>
+                        </div>
+                    </section>
+                </Modal>
+
             </div>
         )
     }    
