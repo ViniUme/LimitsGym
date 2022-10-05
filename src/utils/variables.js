3f7a142cd12cce54e96ef9b123c593ac696c69a9
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-10-05 14:17:35 -0300

    Edit RegEx variable

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -7,10 +7,12 @@ export function Patterns(){
 
 export function RegEx(){
     return {
-        email: new RegExp('[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'),
+        email: new RegExp('(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))'),
         tel: new RegExp(/[\)\(\-\s)]/g),
         rg: new RegExp(/[\.\-]/g),
         password: new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'),
-        space: new RegExp(/\s/g)
+        space: new RegExp(/\s/g),
+        name: new RegExp('^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9_-]{3,60}$'),
+        others: new RegExp('^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9_-]{3,30}$')
     }
 }
