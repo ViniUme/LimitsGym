c0b6682a11e690df3f5741b7b6f5d66b76f3b286
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-10-06 13:37:58 -0300

    Fix name RegEx

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -13,7 +13,7 @@ export function RegEx(){
         password: new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'),
         space: new RegExp(/\s/g),
         more_space: new RegExp(/\s+\s/g),
-        name: new RegExp('^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9_-]{3,60}$')
+        name: new RegExp(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\s]{3,60}$/gm)
     }
 }
 
