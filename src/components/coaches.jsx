de86ee1040988854a3679b2fccfe0ecd716612d8
ViniUme <viniciusumetsu210@gmail.com>
2022-10-09 12:21:22 -0300

    Edit instagram icons

--- src/components/coaches.jsx
+++ src/components/coaches.jsx
@@ -1,5 +1,5 @@
+import InstagramIcon from '@mui/icons-material/Instagram';
 import styles from '../styles/coaches.module.scss';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Coaches(){
@@ -10,28 +10,28 @@ export default function Coaches(){
                 <div className={styles.image_coach_1} />
                 <div className={styles.coache_text}>
                     <span>Gabriel Tardivo</span>
-                    <a className={styles.insta}><Image src="/img/instagram.webp" width={30} height={30} alt="" /> <span className={styles.insta_id}>gabrieltardivo</span></a>
+                    <a className={styles.insta}><InstagramIcon className={styles.image}/> <span className={styles.insta_id}>gabrieltardivo</span></a>
                 </div>
             </a></Link>
             <Link href="https://www.instagram.com/maaxmarlon/"><a target="_blank" className={styles.img_div}>
                 <div className={styles.image_coach_2} />
                 <div className={styles.coache_text}>
                     <span>Max Marlon</span>
-                    <a className={styles.insta}><Image src="/img/instagram.webp" width={30} height={30} alt="" /> <span className={styles.insta_id}>maaxmarlon</span></a>
+                    <a className={styles.insta}><InstagramIcon className={styles.image}/> <span className={styles.insta_id}>maaxmarlon</span></a>
                 </div>
             </a></Link>
             <Link href="https://www.instagram.com/treinador_kasa/"><a target="_blank" className={styles.img_div}>
                 <div className={styles.image_coach_3} />
                 <div className={styles.coache_text}>
                     <span>Marcos Kasa</span>
-                    <a className={styles.insta}><Image src="/img/instagram.webp" width={30} height={30} alt="" /> <span className={styles.insta_id}>treinador_kasa</span></a>
+                    <a className={styles.insta}><InstagramIcon className={styles.image}/> <span className={styles.insta_id}>treinador_kasa</span></a>
                 </div>
             </a></Link>
             <Link href="https://www.instagram.com/julianocruz889/"><a target="_blank" className={styles.img_div}>
                 <div className={styles.image_coach_4} />
                 <div className={styles.coache_text}>
                     <span>Juliano Ricardo</span>
-                    <a className={styles.insta}><Image src="/img/instagram.webp" width={30} height={30} alt="" /> <span className={styles.insta_id}>julianocruz889</span></a>
+                    <a className={styles.insta}><InstagramIcon className={styles.image}/> <span className={styles.insta_id}>julianocruz889</span></a>
                 </div>
             </a></Link>
         </section>
